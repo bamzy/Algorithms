8e3f2911bf027dcdfcf59f7974699b2c24e0bf6a
Bamdad <aghilide@ualberta.ca>
2020-02-08 21:17:32 -0500

    finished longest consecutive subset. Tested

--- src/com/bamzy/LongestConsecutiveSequence.java
+++ src/com/bamzy/LongestConsecutiveSequence.java
@@ -1,4 +1,45 @@
 package com.bamzy;
-    /*This class returns the longest consecutive set of elements in an array*/
+
+import java.util.HashMap;
+
+/*This class returns the longest consecutive set of elements in an array*/
 public class LongestConsecutiveSequence {
+    public int getLongestSubsetSize(int[] input){
+        int maxSize = 0;
+        int tempSize;
+        HashMap<Integer,Boolean> list = new HashMap<>();
+        // Initialize the hashmap
+        for (int value : input) {
+            list.put(value,false);
+        }
+
+        // Find elements that are smaller than something
+        for (int value : input) {
+            if (list.containsKey(value+1)  )
+                list.put(value,true);
+        }
+
+        // Find elements that are bigger that something
+        for (int value : input) {
+            if (list.containsKey(value-1) )
+                list.put(value,false);
+        }
+        for (int value : input) {
+            tempSize = 0;
+            int temp = value;
+            if (list.get(value) == true )
+                while (true){
+                    if (list.containsKey(temp+1) ) {
+                        tempSize++;
+                        temp++;
+                    } else {
+                        break;
+                    }
+                }
+                if (tempSize > maxSize)
+                    maxSize = tempSize;
+        }
+
+        return maxSize;
+    }
 }
--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -5,8 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         SortedSquareArray sortedSquareArray = new SortedSquareArray();
+        LongestConsecutiveSequence longestConsecutiveSequence = new LongestConsecutiveSequence();
         int[] a = new int[]{-10,-9,-5,-3,1,2,3,4};
         sortedSquareArray.sortSquared(a);
         sortedSquareArray.printResult();
+
+        int[] b = new int[]{2,1,3,7,4,5,99,17,8};
+        System.out.println(longestConsecutiveSequence.getLongestSubsetSize(b));
     }
 }
