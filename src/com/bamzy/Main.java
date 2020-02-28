59edb41abcf56cd8018efa6dbc19e61936e126fd
Bamdad <aghilide@ualberta.ca>
2020-02-28 18:42:08 -0500

    some minor practice to show that Java is always pass by value

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -211,16 +211,29 @@ public class Main {
 //        a.left = b; a.right = c; b.left =d;b.right = e; c.left = f; c.right = g;
 //        findTreePathBySum.findTreePathEqualToSum(a,8);
 
+//
+//        TreeNode a = new TreeNode(1);
+//        TreeNode b = new TreeNode(2);
+//        TreeNode c = new TreeNode(3);
+//        TreeNode d = new TreeNode(4);
+//        TreeNode e = new TreeNode(5);
+//        TreeNode f = new TreeNode(6);
+//        TreeNode g = new TreeNode(7);
+//        a.left = b; a.right = c; b.left =d;b.right = e; c.left = f; c.right = g;
+//        System.out.println( findTreePathBySum.hasPathSum(a,6));
+        TreeNode t = new TreeNode(3);
+        int[] test = new int[]{1,2,3};
+        Integer b = 3;
+        String c = "aaaaa";
+        doSOmething(test, b,t,c);
+        System.out.println(test);
 
-        TreeNode a = new TreeNode(1);
-        TreeNode b = new TreeNode(2);
-        TreeNode c = new TreeNode(3);
-        TreeNode d = new TreeNode(4);
-        TreeNode e = new TreeNode(5);
-        TreeNode f = new TreeNode(6);
-        TreeNode g = new TreeNode(7);
-        a.left = b; a.right = c; b.left =d;b.right = e; c.left = f; c.right = g;
-        System.out.println( findTreePathBySum.hasPathSum(a,6));
+    }
 
+     static void doSOmething(int[] test, Integer value, TreeNode t, String c) {
+        test[0] = 4;
+        value = 4;
+        t.val = 4;
+        c= "bbbbb";
     }
 }
