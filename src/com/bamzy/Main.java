ce99ed69b97efb24c0d4ecfe8de419d10a9bdf4f
Bamdad <aghilide@ualberta.ca>
2020-02-28 21:12:51 -0500

    a working 3sum but with duplicate results

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -3,17 +3,7 @@ package com.bamzy;
 public class Main {
 
     public static void main(String[] args) {
-
-        Tree<Integer> a = new Tree<>(0);
-        Tree<Integer> b = new Tree<>(1);
-        Tree<Integer> c = new Tree<>(2);
-        Tree<Integer> d = new Tree<>(3);
-        Tree<Integer> e = new Tree<>(4);
-        a.left = b; a.right = c;  c.left = d; c.right = e;
-        ShortestDepthTree shortestDepthTree = new ShortestDepthTree();
-        int[] res= shortestDepthTree.findShortestLongestDepthInTree(a);
-        System.out.println("Min is: " + res[0] + "\n" +
-                "Max is: " + res[1]
-        );
+        ThreeSum threeSum = new ThreeSum();
+        threeSum.findThreeSum(new int[]{-1, 0, 1, 2, -1, -4});
     }
 }
--- src/com/bamzy/ThreeSum.java
+++ src/com/bamzy/ThreeSum.java
@@ -11,10 +11,34 @@ A solution set is:
   [-1, 0, 1],
   [-1, -1, 2]
 ]*/
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ThreeSum {
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> findThreeSum(int[] nums) {
+        HashMap<Integer, Integer> numbers= new HashMap<>();
+        List<List<Integer>> results = new ArrayList<>();
+        int tempSum;
+        for (int num : nums) {
+            if (numbers.containsKey(num)){
+                numbers.put(num, numbers.get(num)+1);
+            } else
+                numbers.put(num, 1);
+        }
+        for (int i = 0 ; i < nums.length - 1 ; i++){
+            for (int j = +1 ; j < nums.length ; j++){
+                tempSum = nums[i]+nums[j];
+                if (numbers.containsKey(-tempSum)){
+                    ArrayList<Integer> node = new ArrayList<>();
+                    node.add(nums[i]);
+                    node.add(nums[j]);
+                    node.add(-tempSum);
+                    results.add(node);
+                }
+            }
+        }
+        return results;
 
     }
 }
