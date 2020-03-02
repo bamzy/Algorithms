e46650bd8161b89c2d7b4e19b9e890f2c0ff21b5
Bamdad <aghilide@ualberta.ca>
2020-03-02 14:24:14 -0500

    a working code that sort strings based on character frequency

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -3,8 +3,8 @@ package com.bamzy;
 public class Main {
 
     public static void main(String[] args) {
-        SetMatrixZeroes setMatrixZeroes = new SetMatrixZeroes();
-        setMatrixZeroes.inPlaceSetZeroes(new int[][]{{0,1,2,0}, {3,4,5,2}, {1,3,1,5}});
 
+        SortCharsByOccurence sortCharsByOccurence = new SortCharsByOccurence();
+        System.out.println(sortCharsByOccurence.sortByFrequency("abcadebfccg"));
     }
 }
