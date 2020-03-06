edb32c461e01d808c1f0c8ebb31e93b7407af6d1
Bamdad <aghilide@ualberta.ca>
2020-03-06 15:41:20 -0500

    a very fast TwoSum

---
+++ src/com/bamzy/TowSum.java
@@ -0,0 +1,18 @@
+package com.bamzy;
+
+import java.util.HashMap;
+
+public class TowSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0 ; i< nums.length; i++){
+            if (!map.containsKey(nums[i]))
+                map.put(nums[i],i);
+            Integer index = map.get(target - nums[i]);
+            if ( index != null && index != i ) {
+                return new int[]{i, index};
+            }
+        }
+        return null;
+    }
+}
