b8a53b0c3ca1b106681c312b7d6884d5c3b834d8
Bamdad <aghilide@ualberta.ca>
2020-03-07 20:41:02 -0500

    WIP

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -3,17 +3,18 @@ package com.bamzy;
 public class Main {
 
     public static void main(String[] args) {
-        LListNode a = new LListNode(1);
-        LListNode b = new LListNode(2);
-        LListNode c = new LListNode(3);
+        LListNode a = new LListNode(4);
+        LListNode b = new LListNode(1);
+        LListNode c = new LListNode(8);
         LListNode d = new LListNode(4);
         LListNode e = new LListNode(5);
-        LListNode f = new LListNode(6);
-        LListNode g = new LListNode(7);
-        LListNode h = new LListNode(8);
+        LListNode f = new LListNode(5);
+        LListNode g = new LListNode(0);
+        LListNode h = new LListNode(1);
 
-        a.next = b; b.next = c; c.next = d; d.next = e; e.next = f; f.next =g;g.next = h;
-        OddEvenLinkedLists oddEvenLinkedLists = new OddEvenLinkedLists();
-        oddEvenLinkedLists.oddEvenList(a);
+        a.next = b; b.next = c; c.next = d; d.next = e;
+        f.next =g;g.next = h; h.next = c;
+        IntersectionOfLinkedLists intersectionOfLinkedLists = new IntersectionOfLinkedLists();
+        intersectionOfLinkedLists.getIntersectionNode(a,f);
     }
 }
