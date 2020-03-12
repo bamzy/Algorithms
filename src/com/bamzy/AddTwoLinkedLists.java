c623394ffc8b4a1f37052b0ba848776350a2708a
Bamdad <aghilide@ualberta.ca>
2020-03-12 14:26:58 -0400

    yet another sum of linked lists that are for some reason reversed.

---
+++ src/com/bamzy/AddTwoLinkedLists.java
@@ -0,0 +1,36 @@
+package com.bamzy;
+
+public class AddTwoLinkedLists {
+    public LListNode addTwoNumbers(LListNode l1, LListNode l2) {
+        LListNode temp = l1,prev1 = null, prev2=null, next1,next2,prev;
+        int carryOver = 0;
+        prev1 = l1;
+        prev2= l2;
+        LListNode head = new LListNode(0);
+        temp = head;
+        while (prev1 != null || prev2!=null || carryOver !=0){
+            Integer sum = 0;
+            if (prev1!=null && prev2!= null) {
+                sum = prev1.val + prev2.val + carryOver;
+            } else if (prev1 == null && prev2 != null) {
+                sum = prev2.val + carryOver;
+            } else if (prev1 != null && prev2 == null) {
+                sum = prev1.val + carryOver;
+            } else if (prev1 == null && prev2 == null && carryOver != 0) {
+                sum = carryOver;
+            }
+            if (sum > 9)
+                carryOver = 1;
+            else carryOver = 0;
+            prev = new LListNode(sum%10);
+            temp.next =  prev;
+            temp = temp.next;
+            if (prev1 != null)
+                prev1 = prev1.next;
+            if (prev2 != null)
+                prev2 = prev2.next;
+        }
+        LListNode temp2 = head.next;
+        return temp2;
+    }
+}
--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -3,7 +3,22 @@ package com.bamzy;
 public class Main {
 
     public static void main(String[] args) {
-       StringToInteger stringToInteger = new StringToInteger();
-        System.out.println(stringToInteger.myAtoi("+-2"));
+        LListNode a = new LListNode(2);
+        LListNode b = new LListNode(4);
+        LListNode c = new LListNode(3);
+        a.next = b; b.next =c; c.next = null;
+        LListNode d = new LListNode(5);
+        LListNode e = new LListNode(6);
+        LListNode f = new LListNode(4);
+        d.next = e; e.next = f; f.next = null;
+
+        LListNode g = new LListNode(1);
+        LListNode h = new LListNode(8);
+        LListNode i = new LListNode(0);
+        g.next = h; h.next = null; i.next = null;
+
+       AddTwoLinkedLists addTwoLinkedLists = new AddTwoLinkedLists();
+       addTwoLinkedLists.addTwoNumbers(a,d);
+       addTwoLinkedLists.addTwoNumbers(g,i);
     }
 }
