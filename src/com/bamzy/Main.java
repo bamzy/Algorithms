61935ae3ad9b4f367c46056e6a50dbf2e03c1263
Bamdad <aghilide@ualberta.ca>
2020-03-12 13:23:26 -0400

    a working Atoi that handles Int.Max/Int.Min

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -3,18 +3,7 @@ package com.bamzy;
 public class Main {
 
     public static void main(String[] args) {
-        LListNode a = new LListNode(4);
-        LListNode b = new LListNode(1);
-        LListNode c = new LListNode(8);
-        LListNode d = new LListNode(4);
-        LListNode e = new LListNode(5);
-        LListNode f = new LListNode(5);
-        LListNode g = new LListNode(0);
-        LListNode h = new LListNode(1);
-
-        a.next = b; b.next = c; c.next = d; d.next = e;
-        f.next =g;g.next = h; h.next = c;
-        IntersectionOfLinkedLists intersectionOfLinkedLists = new IntersectionOfLinkedLists();
-        System.out.println(intersectionOfLinkedLists.getIntersectionNode(a,f).val);
+       StringToInteger stringToInteger = new StringToInteger();
+        System.out.println(stringToInteger.myAtoi("+-2"));
     }
 }
