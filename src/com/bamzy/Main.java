4f755d93284013ccb6ce7afd1fdc7b9bf633f2a3
Bamdad <aghilide@ualberta.ca>
2020-03-19 18:02:38 -0400

    WIP: there seems to be a simple bug

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -3,21 +3,21 @@ package com.bamzy;
 public class Main {
 
     public static void main(String[] args) {
-//        TreeNode a = new TreeNode(5);
-//        TreeNode b = new TreeNode(3);
-//        TreeNode c = new TreeNode(6);
-//        TreeNode d = new TreeNode(2);
-//        TreeNode e = new TreeNode(4);
-//        TreeNode f = new TreeNode(1);
-        TreeNode a = new TreeNode(1);
-        TreeNode b = new TreeNode(2);
-        TreeNode c = new TreeNode(3);
-        TreeNode d = new TreeNode(4);
-        TreeNode e = new TreeNode(5);
-        a.left =b; a.right = c; b.left=d; b.right = e;
+//        TreeNode a = new TreeNode(1);
+//        TreeNode b = new TreeNode(2);
+//        TreeNode c = new TreeNode(3);
+//        TreeNode d = new TreeNode(4);
+//        TreeNode e = new TreeNode(5);
+//        TreeNode f = new TreeNode(6);
+//        TreeNode g = new TreeNode(7);
+
+        TreeNode a = new TreeNode(3);
+        TreeNode b = new TreeNode(9);
+        TreeNode c = new TreeNode(20);
+        TreeNode d = new TreeNode(15);
+        TreeNode e = new TreeNode(7);
+        a.left =b; a.right = c;  c.left = d; c.right = e;
         XOrderTraversal xOrderTraversal = new XOrderTraversal();
-        System.out.println("Inorder: "+ xOrderTraversal.inorderTraversal(a));
-        System.out.println("Preorder: "+ xOrderTraversal.preorderTraversal(a));
-        System.out.println("Postorder: "+xOrderTraversal.postorderTraversal(a));
+        System.out.println("Zigzag: "+xOrderTraversal.zigzagLevelOrder(a));
     }
 }
--- src/com/bamzy/XOrderTraversal.java
+++ src/com/bamzy/XOrderTraversal.java
@@ -1,7 +1,6 @@
 package com.bamzy;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class XOrderTraversal {
     public List<Integer> inorderTraversal(TreeNode root) {
@@ -39,6 +38,43 @@ public class XOrderTraversal {
         postOrder(root.left,res);
         postOrder(root.right,res);
         res.add(root.val);
+    }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        List<TreeNode> queue = new ArrayList<>();
+        boolean directionFlag = true;
+
+        queue.add(root);
+        while (!queue.isEmpty()){
+            List<Integer> temp = new ArrayList<>();
+            List<TreeNode> t1 = new ArrayList<>();
+            if (directionFlag) {
+                for (TreeNode val : queue) {
+                    temp.add(val.val);
+                    if (directionFlag)
+                    if (val.left!=null)
+                        t1.add(val.left);
+                    if (val.right!=null)
+                        t1.add(val.right);
+                }
+            } else {
+                for (int i = queue.size()-1; i>=0; i--){
+                    temp.add(queue.get(i).val);
+
+                    if (queue.get(i).right!=null)
+                        t1.add(queue.get(i).right);
+                    if (queue.get(i).left!=null)
+                        t1.add(queue.get(i).left);
+                }
+            }
+            queue.clear();
+            queue.addAll(t1);
+            result.add(temp);
+            directionFlag = !directionFlag;
+        }
+        return result;
     }
 
 }
