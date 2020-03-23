0a2982f8e3c1d006946f00e739ad2a0a796223bb
Bamdad <aghilide@ualberta.ca>
2020-03-23 17:01:53 -0400

    turn pre-order + in-order into tree

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -3,13 +3,16 @@ package com.bamzy;
 public class Main {
 
     public static void main(String[] args) {
-        TreeNode a = new TreeNode(1);
-        TreeNode b = new TreeNode(2);
-        TreeNode c = new TreeNode(3);
-        TreeNode d = new TreeNode(4);
-        TreeNode e = new TreeNode(5);
-        a.left = b; a.right = c; c.left = d; c.right = e;
+//        TreeNode a = new TreeNode(1);
+//        TreeNode b = new TreeNode(2);
+//        TreeNode c = new TreeNode(3);
+//        TreeNode d = new TreeNode(4);
+//        TreeNode e = new TreeNode(5);
+//        a.left = b; a.right = c; c.left = d; c.right = e;
         SerializeDeserializeBST codec = new SerializeDeserializeBST();
-        System.out.println(codec.serialize(a));
+        codec.constructTreeFromPreOrderInOrder(new String[]{"4","2","5","1","6","3"},new String[]{"1","2","4","5","3","6"});
+
+
+
     }
 }
--- src/com/bamzy/SerializeDeserializeBST.java
+++ src/com/bamzy/SerializeDeserializeBST.java
@@ -36,4 +36,46 @@ public class SerializeDeserializeBST {
     public TreeNode deserialize(String data) {
         return null;
     }
+
+    public TreeNode constructTreeFromPreOrderInOrder(String[] inOrder, String[] preOrder){
+        int len = preOrder.length;
+        TreeNode root = buildTree(inOrder, preOrder, 0, len - 1);
+        return  root;
+    }
+    static int preIndex = 0;
+    TreeNode buildTree(String[] inOrder, String[] preOrder, int inStart, int inEnd)
+    {
+        if (inStart > inEnd)
+            return null;
+
+        /* Pick current node from Preorder traversal using preIndex
+           and increment preIndex */
+        TreeNode tNode = new TreeNode(Integer.parseInt(preOrder[preIndex++]));
+
+        /* If this node has no children then return */
+        if (inStart == inEnd)
+            return tNode;
+
+        /* Else find the index of this node in Inorder traversal */
+        int inIndex = search(inOrder, inStart, inEnd, tNode.val);
+
+        /* Using index in Inorder traversal, construct left and
+           right subtress */
+        tNode.left = buildTree(inOrder, preOrder, inStart, inIndex - 1);
+        tNode.right = buildTree(inOrder, preOrder, inIndex + 1, inEnd);
+
+        return tNode;
+    }
+    /* Function to find index of value in arr[start...end]
+   The function assumes that value is present in in[] */
+    int search(String[] arr, int start, int end, int value)
+    {
+        int i;
+        for (i = start; i <= end; i++) {
+            if (Integer.parseInt(arr[i]) == value)
+                return i;
+        }
+        return i;
+    }
+
 }
