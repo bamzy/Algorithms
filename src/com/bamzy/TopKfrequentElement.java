ddc580ecedebaf2b4365fdd06e0f4878db3e87a8
Bamdad <aghilide@ualberta.ca>
2020-03-25 22:32:35 -0400

    a faster way with O(n) using buckets

--- src/com/bamzy/TopKfrequentElement.java
+++ src/com/bamzy/TopKfrequentElement.java
@@ -19,4 +19,27 @@ public class TopKfrequentElement {
         while(k--!=0) result.add(queue.poll().getKey());
         return result;
     }
+
+    public List<Integer> topKFrequent1(int[] nums, int k) {
+        HashMap<Integer, Integer> countsToNumbers = new HashMap<>();
+        List<Integer>[] bucket = new List[nums.length+1];
+        for (int num : nums) {
+                countsToNumbers.put(num, countsToNumbers.getOrDefault(num,0) + 1);
+        }
+        for (int key : countsToNumbers.keySet()) {
+            int frequency = countsToNumbers.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+        List<Integer> res = new ArrayList<>();
+
+        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+            if (bucket[pos] != null) {
+                res.addAll(bucket[pos]);
+            }
+        }
+        return res;
+    }
 }
