2840c38c35c912722c0127481dbc1d06748265b5
Bamdad <aghilide@ualberta.ca>
2020-03-26 00:13:02 -0400

    the easiest problem ever to find the Kth largest element

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-       ConstructBinaryFromPreorderAndInorder constructBinaryFromPreorderAndInorder = new ConstructBinaryFromPreorderAndInorder();
-       constructBinaryFromPreorderAndInorder.buildTree(new int[]{1}, new int[]{1});
+       KthLargestElementInArray kthLargestElementInArray = new KthLargestElementInArray();
+       kthLargestElementInArray.findKthLargest(new int[]{3,2,3,1,2,4,5,5,6},4);
     }
 }
