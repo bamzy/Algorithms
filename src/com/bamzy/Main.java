c4c2c9f1c19f5e6d2bedf870c7cea78f1b2a398e
Bamdad <aghilide@ualberta.ca>
2020-03-25 22:47:03 -0400

    works but doesn't pass leetcode test

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -5,8 +5,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-       TopKfrequentElement topKfrequentElement = new TopKfrequentElement();
-        var integers = topKfrequentElement.topKFrequent(new int[]{1, 0, 2, 4, 2, 1, 4, 3, 0}, 3);
-        System.out.printf("integers");
+       ConstructBinaryFromPreorderAndInorder constructBinaryFromPreorderAndInorder = new ConstructBinaryFromPreorderAndInorder();
+       constructBinaryFromPreorderAndInorder.buildTree(new int[]{1}, new int[]{1});
     }
 }
