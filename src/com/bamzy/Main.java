be134121c5727659fc24c9054057ff007d0f04ef
Bamdad <aghilide@ualberta.ca>
2020-04-21 21:21:17 -0400

    weirdly this works as LRU cache

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -1,24 +1,19 @@
 package com.bamzy;
 
-import java.util.List;
+import java.util.*;
 
 public class Main {
-
+    // Refers to the max size of the map following which
+    // the removal takes place of the eldest entry
+    private static final int MAX = 6;
     public static void main(String[] args) {
-        LListNode a = new LListNode(1);
-        LListNode b = new LListNode(2);
-        LListNode c = new LListNode(3);
-        LListNode d = new LListNode(4);
-        LListNode e = new LListNode(5);
-        LListNode f = new LListNode(6);
-        LListNode g = new LListNode(7);
-        LListNode h = new LListNode(8);
-        a.next = b; b.next = c; c.next = d; d.next = e; e.next = f; f.next = g; ;
-       RevereseKgroupLinkedList revereseKgroupLinkedList = new RevereseKgroupLinkedList();
-       LListNode res = revereseKgroupLinkedList.reverseKGroup(a,3);
-       LListNode res1 = revereseKgroupLinkedList.reverseKGroup(a,2);
-       g.next = h;
-       LListNode res2 = revereseKgroupLinkedList.reverseKGroup(a,3);
-       return;
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.put(1,1);
+        lruCache.put(2,2);
+        lruCache.put(3,3);
+        System.out.println(lruCache.get(2));
+        lruCache.put(4,4);
+        System.out.println(lruCache.get(2));
+
     }
 }
