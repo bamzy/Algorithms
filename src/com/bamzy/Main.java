5337979ca0f8420baaa22c22761827296b5c6ce8
Bamdad <aghilide@ualberta.ca>
2020-04-23 01:57:58 -0400

    a working bucket sort

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -1,25 +1,8 @@
 package com.bamzy;
 
-
 public class Main {
-
-        public static void main(String[] args) {
-            Runtime.Version version = Runtime.Version.parse("10.0.1");
-            printVersionInfo(version);
-            String versionDate = System.getProperty("java.version.date");
-            System.out.println("java.version.date: " + versionDate);
-
-            String vendorVersion = System.getProperty("java.vendor.version");
-            System.out.println("java.vendor.version: " + vendorVersion);
-        }
-
-        private static void printVersionInfo(Runtime.Version version) {
-            int feature = version.feature();
-            int interim = version.interim();
-            int update = version.update();
-            int patch = version.patch();
-            System.out.printf(" feature: %s%n interim: %s%n update: %s%n patch: %s%n",
-                    feature, interim, update, patch);
-        }
-
+    public static void main(String[] args) {
+           BucketSort bucketSort = new BucketSort();
+        System.out.println(bucketSort.sort(new double[]{0.78,0.17,0.39,0.26,0.72,0.94,0.21,0.33,0.68},10));
+    }
 }
