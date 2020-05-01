06e6f6de5fa11b8317ebe1121353b112e59c6998
Bamdad <aghilide@ualberta.ca>
2020-05-01 17:44:37 -0400

    This needs further investigations

--- src/com/bamzy/KClosestPointToOrigin.java
+++ src/com/bamzy/KClosestPointToOrigin.java
@@ -11,9 +11,10 @@ public class KClosestPointToOrigin {
     // we'll reduce the problem to a problem of half the size.
     // O(n) + O(n/2) + ... O(1) = O(2n)
     public int[][] kClosest(int[][] points, int K) {
-        int len =  points.length, l = 0, r = len - 1;
+        int r =  points.length-1 , l = 0;
         while (l <= r) {
-            int mid = helper(points, l, r);
+//            int mid = helper(points, l, r);
+            int mid = helper1(points, l, r);
             if (mid == K) break;
             if (mid < K) {
                 l = mid + 1;
@@ -39,6 +40,33 @@ public class KClosestPointToOrigin {
         }
         A[l] = pivot; // Finally put the pivot at l
         return l;
+    }
+
+    private int helper1(int[][] A, int l, int r) {
+        int[] pivot = A[l];
+        int dist = dist(pivot);
+
+        int i = (l-1); // index of smaller element
+        for (int j=l; j<r; j++)
+        {
+            // If current element is smaller than the pivot
+            if (dist(A[j]) < dist)
+            {
+                i++;
+
+                // swap arr[i] and arr[j]
+                int[] temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        int[] temp = A[i+1];
+        A[i+1] = A[r];
+        A[r] = temp;
+
+        return i+1;
     }
 
     private int dist(int[] point) {
