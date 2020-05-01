7074a2b750ce68427a7cc430dfe2d5df4c0d1b83
Bamdad <aghilide@ualberta.ca>
2020-05-01 19:06:00 -0400

    a response that is in a top 82% answers

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -4,16 +4,8 @@ package com.bamzy;
 public class Main {
 
     public static void main(String[] args) {
-        TreeNode a = new TreeNode(99911);
-        TreeNode b = new TreeNode(22);
-        TreeNode c = new TreeNode(33);
-        TreeNode d = new TreeNode(44);
-        TreeNode e = new TreeNode(55);
-        TreeNode f = new TreeNode(55566);
-        a.left = b; a.right = c; b.left = d; c.right = e; d.left = f;
-        SerializeDeserializeBST sds = new SerializeDeserializeBST();
-        TreeNode res = sds.deserialize(sds.serialize(a));
-
+        HappyNumber happyNumber = new HappyNumber();
+        System.out.println(happyNumber.isHappy(19));
 
     }
 }
