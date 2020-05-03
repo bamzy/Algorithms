378fa3634899e5806252df78863dd8af6519db8a
Bamdad <aghilide@ualberta.ca>
2020-05-02 20:14:27 -0400

    A working title converter for excel

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -4,10 +4,9 @@ package com.bamzy;
 public class Main {
 
     public static void main(String[] args) {
-        FactorialTrailingZeroes ftz = new FactorialTrailingZeroes();
-        System.out.println(ftz.trailingZeroes(3));
-        System.out.println(ftz.trailingZeroes(5));
-        System.out.println(ftz.trailingZeroes(30));
+        ExcelSheetColumnNumber excn = new ExcelSheetColumnNumber();
+
+        System.out.println(excn.titleToNumber("ZY"));
 
     }
 }
