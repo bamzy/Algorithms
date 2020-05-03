9e1f725d548240cdc6606eee08da898805b3c24b
Bamdad <aghilide@ualberta.ca>
2020-05-02 20:46:55 -0400

    A really interesting recursive pow function

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -4,9 +4,8 @@ package com.bamzy;
 public class Main {
 
     public static void main(String[] args) {
-        ExcelSheetColumnNumber excn = new ExcelSheetColumnNumber();
-
-        System.out.println(excn.titleToNumber("ZY"));
+        Pow pow = new Pow();
+        System.out.println(pow.myPow(2,-2147483648));
 
     }
 }
