ac21c05265e6a30900a60498928dc750bffcc8b6
Bamdad <aghilide@ualberta.ca>
2020-05-04 21:38:24 -0400

    my version plus an above 97% version

--- src/com/bamzy/CoinChange.java
+++ src/com/bamzy/CoinChange.java
@@ -2,6 +2,7 @@ package com.bamzy;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 /*
 You are given coins of different denominations and a
@@ -22,31 +23,45 @@ Note:
 You may assume that you have an infinite number of each kind of coin.
 */
 public class CoinChange {
-    int minCoins = Integer.MAX_VALUE;
     public int coinChange(int[] coins, int amount) {
+        int[] res = new int[amount+1];
         Arrays.sort(coins);
-        reverse(coins);
-        rec(coins,amount,0);
-        return (minCoins == Integer.MAX_VALUE) ? -1 : minCoins;
-    }
-    public static void reverse(int[] input) {
-        int last = input.length - 1;
-        int middle = input.length / 2;
-        for (int i = 0; i <= middle; i++) {
-            int temp = input[i]; input[i] = input[last - i];
-            input[last - i] = temp;
+        res[0] = 0;
+        for (int i = 1; i < res.length ; i++) {
+            res[i] = findMin(res,i,coins);
         }
+        return res[res.length-1];
+    }
+    public int coinChange1(int[] c, int m) {
+
+        int[] a = new int[m+1];
+        for(int i = 1; i<=m; i++){
+            int min = m+1;
+            for(int j=0; j<c.length; j++){
+                if(i>=c[j]){
+                    min = Math.min(min, a[i-c[j]] + 1);
+                }
+            }
+            a[i] = min;
+        }
+        return a[m] == m+1 ? -1 : a[m];
     }
 
-
-
-    private void rec(int[] coins, int amount, int coinNum) {
-        if (amount == 0)
-            minCoins = Math.min(minCoins,coinNum);
-        if (amount<0)
-            return;
+    private int findMin(int[] res, int currentIndex, int[] coins) {
+        int min = Integer.MAX_VALUE;
         for (int coin : coins) {
-            rec(coins, amount-coin,coinNum+1);
+            if (currentIndex-coin >= 0){
+                if (currentIndex-coin == 0)
+                    min = Math.min(min,1);
+                else if (currentIndex-coin > 0 ) {
+                    if (res[currentIndex-coin] != -1)
+                        min = Math.min(min, res[currentIndex - coin] + 1);
+
+                }
+            } else
+                break;
         }
+        return (min == Integer.MAX_VALUE) ? -1 : min;
     }
+
 }
--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
         CoinChange coinChange = new CoinChange();
-        System.out.println(coinChange.coinChange(new int[]{1,2,5},11));
+//        System.out.println(coinChange.coinChange(new int[]{1,2,5},11));
+//        System.out.println(coinChange.coinChange(new int[]{2},3));
+        System.out.println(coinChange.coinChange(new int[]{186,419,83,408},6249));
     }
 }
