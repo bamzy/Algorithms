1296cfaea5f2961d6ec2ad553d232561f2467eb1
Bamdad <aghilide@ualberta.ca>
2020-05-05 22:14:11 -0400

    not much changed, I'll leave it as is for now

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -4,9 +4,9 @@ package com.bamzy;
 public class Main {
 
     public static void main(String[] args) {
-        CoinChange coinChange = new CoinChange();
-//        System.out.println(coinChange.coinChange(new int[]{1,2,5},11));
-//        System.out.println(coinChange.coinChange(new int[]{2},3));
-        System.out.println(coinChange.coinChange(new int[]{186,419,83,408},6249));
+        LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
+        System.out.println(lis.lengthOfLIS(new int[]{10,9,2,5,3,4}));
+        System.out.println(lis.lengthOfLIS(new int[]{10}));
+        System.out.println(lis.lengthOfLIS(new int[]{10,9,2,5,3,7,101,18}));
     }
 }
