ac80ad96941c22e32cc83e5e135f4aa874878d10
Bamdad <aghilide@ualberta.ca>
2020-05-08 22:07:31 -0400

    haha this one is beyond me

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -1,10 +1,13 @@
 package com.bamzy;
 
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         Subset subset = new Subset();
-        subset.subsets(new int[]{1,2,3,0});
+        var subsets = subset.subsets(new int[]{1, 2, 3});
+        System.out.println(subsets);
     }
 }
--- src/com/bamzy/Subset.java
+++ src/com/bamzy/Subset.java
@@ -4,32 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Subset {
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsets1(int[] nums) {
+        // start with [[]]
+        List<List<Integer>> ans = new ArrayList();
+        ans.add(new ArrayList<Integer>());
 
-        if (nums.length == 0)
-            return null;
+        // keep adding each input number to all the sub-set till now
+        for(int num : nums ){
+            //subsets till now iteration one by one
+            int n = ans.size();
+            for(int j = 0 ; j<n; j++){
+                List<Integer> each = new ArrayList(ans.get(j));
+                each.add(num);
+                ans.add(each);
+            }
+        }
+        return ans;
+
+    }
+    // TC: O(2 ^ N * N)
+    // SC: O(2 ^ N * N)
+    public List<List<Integer>> subsets(int[] nums) {
+        if (nums == null) {
+            return new ArrayList<>();
+        }
+
         List<List<Integer>> res = new ArrayList<>();
-        rec(0,nums,res);
+        List<Integer> subset = new ArrayList<>();
+
+        helper(subset, nums, 0, res);
         return res;
     }
-    public void rec(int start, int[] nums, List<List<Integer>> res){
-        if (start == nums.length){
-            List<Integer> temp = new ArrayList<>();
-            for (int num : nums) {
-                temp.add(num);
-            }
-            res.add(temp);
+
+    private void helper(List<Integer> subset,
+                        int[] nums,
+                        int startIndex,
+                        List<List<Integer>> res) {
+        res.add(new ArrayList<Integer>(subset));
+        for (int i = startIndex; i < nums.length; i++) {
+            subset.add(nums[i]);
+            helper(subset, nums, i + 1, res);
+            subset.remove(subset.size() - 1);
         }
-        for (int i=start ; i< nums.length; i++){
-            swap(i,start,nums);
-            rec(start+1,nums,res);
-            swap(start,i,nums);
-        }
+
     }
 
-    private void swap(int i, int start, int[] nums) {
-        int temp = nums[i];
-        nums[i] = nums[start];
-        nums[start] = temp;
-    }
 }
