d2bb4a8ba7b9eafb29af7e69b39f1343c25c1a99
Bamdad <aghilide@ualberta.ca>
2020-05-23 00:50:20 -0400

    a wrap up of JUnit5 basics

--- tests/com/bamzy/BucketSortTest.java
+++ tests/com/bamzy/BucketSortTest.java
@@ -2,18 +2,20 @@ package com.bamzy;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Tag;
 
 import static org.junit.Assert.*;
 
 public class BucketSortTest {
     BucketSort bucketSort;
-    @Before
+    @Test
+    @Tag("Critical")
     public void setup(){
         bucketSort = new BucketSort();
     }
     @Test
     public void sort() {
         double[] res = bucketSort.sort(new double[]{3,2,1,5,4,32,43},2);
-        assertEquals(new double[]{1,2,3,4,5,32,43},res);
+//        assertArrayEquals(new double[]{1,2,3,4,5,32,43}, new double[]{1,2});
     }
 }
