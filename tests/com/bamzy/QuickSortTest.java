aecb6ad1d250bd1dd626681605b8f656a84db75b
Bamdad <aghilide@ualberta.ca>
2020-05-23 00:16:41 -0400

    this is how we could use assertAll

--- tests/com/bamzy/QuickSortTest.java
+++ tests/com/bamzy/QuickSortTest.java
@@ -2,7 +2,10 @@ package com.bamzy;
 
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledIf;
 
+import static org.junit.Assume.assumeThat;
+import static org.junit.Assume.assumeTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -44,7 +47,14 @@ class QuickSortTest {
     }
     @AfterAll
     @Test
+    @Tag ("aaa")
     void afterAllDone() {
+        assumeTrue(true);
+        assertAll(
+                () -> assertEquals(1,quickSort.sort(new int[]{1})),
+                () -> assertEquals(1,quickSort.sort(new int[]{1,2})),
+                () -> assertEquals(1,quickSort.sort(new int[]{1,3}))
+        );
         System.out.println("We're all done");
     }
 }
