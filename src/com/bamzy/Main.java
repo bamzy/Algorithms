52798f56bdac6dce1bffb7875bfa05e4107da360
Bamdad <aghilide@ualberta.ca>
2020-06-05 00:39:59 -0400

    finds median in an array with a moving window

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -1,17 +1,18 @@
 package com.bamzy;
 
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        WordSearch ws = new WordSearch();
+        MovingMedian movingMedian = new MovingMedian();
+        System.out.println(movingMedian.ArrayChallenge(new int[]{ 3, 1, 3, 5, 10, 6, 4, 3, 1}));
+        System.out.println(movingMedian.ArrayChallenge(new int[]{ 5,2,4,6}));
+        System.out.println(movingMedian.ArrayChallenge(new int[]{ 1,2,4,6}));
+        System.out.println(movingMedian.ArrayChallenge(new int[]{ 10}));
+        System.out.println(movingMedian.ArrayChallenge(new int[]{ 100,1}));
 
-        System.out.println(ws.exist(new char[][]{{'A', 'B', 'C', 'E'},{'S','F','E','S'},{'A','D','E','E'}},"ABCESEEEFS"));
-        System.out.println(ws.exist(new char[][]{{'A', 'B', 'C', 'E'},{'S','F','C','S'},{'A','D','E','E'}},"ABCCED"));
-        System.out.println(ws.exist(new char[][]{{'A', 'B', 'C', 'E'},{'S','F','C','S'},{'A','D','E','E'}},"SEE"));
-        System.out.println(ws.exist(new char[][]{{'A', 'B', 'C', 'E'},{'S','F','C','S'},{'A','D','E','E'}},"ABCB"));
-        System.out.println(ws.exist(new char[][]{{'A'}},"A"));
     }
 }
