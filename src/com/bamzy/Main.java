f0768f97acdd1f05f09fef9cf082acfbe3ec9810
Bamdad <aghilide@ualberta.ca>
2020-06-05 18:58:09 -0400

    a working custom regex handler

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -8,7 +8,10 @@ public class Main {
 
     public static void main(String[] args) {
         HardRegex hardRegex = new HardRegex();
-        System.out.println(hardRegex.StringChallenge( "$**+*{2} 9mmmrrrkbb"));
+        System.out.println(hardRegex.StringChallenge( "*{1} ")); // false
+        System.out.println(hardRegex.StringChallenge( "+++++* abcdehhhhhh")); // false
+        System.out.println(hardRegex.StringChallenge( "++*{5} jtggggg")); //true
+        System.out.println(hardRegex.StringChallenge( "$**+*{2} 9mmmrrrkbb")); // true
 
     }
 }
