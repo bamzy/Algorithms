1645fdae5a7b3912198f091e0d4f38abcb939a69
Bamdad <bamdad@mortgageautomator.com>
2022-08-25 23:59:24 -0400

    a simple yet brilliant mergesort thanks to the mergetwoarrays function

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -9,5 +9,10 @@ public class Main {
         MergeTwoSortedArrays mrg = new MergeTwoSortedArrays();
         long[] res = mrg.mergetwosortedarrays(new long[]{7},new long[]{1,2,4,6,8,9});
         System.out.println(Arrays.toString(res));
+
+        MergeSort mrgs = new MergeSort();
+        long[] input = {99,5,4,1,0,9999999,99997};
+        long[] res2 = mrgs.solveMergeSort(input);
+        System.out.println(Arrays.toString(res2));
     }
 }
