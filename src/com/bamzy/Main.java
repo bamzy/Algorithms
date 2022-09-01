d7c34b02f628b70e439196c653dbb1ea15d4f1ba
Bamdad <bamdad@mortgageautomator.com>
2022-09-01 00:58:20 -0400

    a simple binary search on an already sorted array

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -14,5 +14,11 @@ public class Main {
         long[] input = {99,5,4,1,0,9999999,99997};
         long[] res2 = mrgs.solveMergeSort(input);
         System.out.println(Arrays.toString(res2));
+
+        TwoStringPermutation tsp = new TwoStringPermutation();
+        tsp.findStringPermutation("abcdeabc","abc");
+
+        BinarySearchLC704 bs = new BinarySearchLC704(new long[]{-1,0,3,5,9,12});
+        System.out.println(bs.findIndexBinarySearch(2));
     }
 }
