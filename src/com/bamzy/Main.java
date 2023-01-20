be6253318076272d2fbcd97418322ed26a052068
Bamdad <bamdad@mortgageautomator.com>
2023-01-20 18:41:00 -0500

    moving on from matrixes section

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -4,6 +4,7 @@ package com.bamzy;
 import com.bamzy.amazon.LongestUniqueStringSplit;
 import com.bamzy.amazon.OverlappingRectangles;
 import com.bamzy.crack_coding_interview.OneAway;
+import com.bamzy.crack_coding_interview.RotateMatrix;
 import com.bamzy.crack_coding_interview.URLify;
 import com.bamzy.google.*;
 
@@ -62,5 +63,6 @@ public class Main {
         URLify.runTest();
 
         OneAway.runTest();
+        RotateMatrix.runTest();
     }
 }
