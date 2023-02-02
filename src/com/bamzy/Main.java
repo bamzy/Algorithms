4da94147fd1527532fe31641db63fb78e372f6dc
Bamdad <bamdad@mortgageautomator.com>
2023-02-02 12:55:03 -0500

    these two algorithms work

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -15,65 +15,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BinaryTreePreorderTraversal.runTest();
-
-        ZigzagStringConversion.runTest();
-
-        AddBinary.runTest();
-
-        TextJustification.runTest();
-
-        SQRTx.runTest();
-
-        ReArrangeSpaceBetweenWords.runTest();
-        MaskingPersonalInfo.runTest();
-        DP_fibbonaci.runTest();
-        DP_tribbonaci.runTest();
-        ReverseInteger.runTest();
-
-        AdjustLicensePlates.runTest();
 
 
-        BuildDirectoryTreeFromString.runTest();
+        LongestSubstringWithKDistinctVals.runTest();
 
-        DP_ClimbStairs.runTest();
-
-        DecideRentByAmenities.runTest();
-
-        RandomStore.runTest();
-
-        ReverseLinkedList.runTest();
-
-        RemoveIslandFromMatrix.runTest();
-
-        Kusaraju.runTest();
-
-        RegularExpression.runTest();
-
-        ValidateSubsequentArrays.runTest();
-
-        GetRiverSizes.runTest();
-
-        OverlappingRectangles.runTest();
-
-        LongestUniqueStringSplit.runTest();
-
-        PascalTriangle.runTest();
-
-        JumpGameII.runTest();
-
-        URLify.runTest();
-
-        OneAway.runTest();
-        RotateMatrix.runTest();
-
-        SnakesAndLadders.runTest();
-
-        KokoBananaEating.runTest();
-
-        SolveSudoku.runTest();
-
-        WeightGainDiet.runTest();
-        MagicTriplet.runTest();
+        FindAllStringAnagrams.runTest();
     }
 }
