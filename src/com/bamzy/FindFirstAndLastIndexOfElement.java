576e09f6d622f56f9f37a92dad12d21518d7f557
Bamdad <bamdad@mortgageautomator.com>
2023-05-15 07:44:16 -0400

    a simple roman alg + a medium rec alg

---
+++ src/com/bamzy/FindFirstAndLastIndexOfElement.java
@@ -0,0 +1,57 @@
+package com.bamzy;
+/*
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+
+
+ */
+public class FindFirstAndLastIndexOfElement {
+    private int earliest = Integer.MAX_VALUE;
+    private int latest = Integer.MIN_VALUE;
+    public int[] searchRange(int[] nums, int target) {
+        recSearch(nums,0,nums.length-1,target);
+        if(earliest == Integer.MAX_VALUE) earliest = -1;
+        if(latest == Integer.MIN_VALUE) latest = -1;
+        return new int[]{earliest,latest};
+    }
+    private void recSearch(int[] nums, int start,int end,int target){
+        if(start > end) return;
+        if (end>=nums.length) return;
+
+        int middleIndex = (start+end)/2;
+        if(nums[middleIndex]>target ){
+            recSearch(nums,start,middleIndex-1,target);
+        } else if (nums[middleIndex]< target){
+            recSearch(nums,middleIndex+1,end,target);
+        } else {
+            earliest = Math.min(middleIndex, earliest);
+            latest = Math.max(middleIndex, latest);
+            recSearch(nums,middleIndex+1,end,target);
+            recSearch(nums,start,middleIndex-1,target);
+        }
+    }
+    public static void runTest(){
+        FindFirstAndLastIndexOfElement f = new FindFirstAndLastIndexOfElement();
+        System.out.println(f.searchRange(new int[]{5,7,7,8,8,10},8));
+        System.out.println(f.searchRange(new int[]{5,7,7,8,8,10},6));
+        System.out.println(f.searchRange(new int[]{},0));
+    }
+}
---
+++ src/com/bamzy/RomanToInteger.java
@@ -0,0 +1,83 @@
+package com.bamzy;
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        Character next,current ;
+        int  sum = 0,skip=0;
+        while( s.length()>0){
+            next = null;
+            current = s.charAt(0);
+            if(s.length()>1) next = s.charAt(1);
+
+            switch(current){
+                case 'I' :
+                    if (next!=null && next == 'V') {
+                        sum += 4;
+                        skip = 2;
+                    }else if (next!=null && next == 'X'){
+                        sum += 9;
+                        skip = 2;
+                    }
+                    else {
+                        sum += 1;
+                        skip = 1;
+                    }
+                    break;
+                case 'X':
+                    if (next!=null && next == 'L') {
+                        sum += 40;
+                        skip = 2;
+                    }else if (next!=null && next == 'C'){
+                        sum += 90;
+                        skip = 2;
+                    }
+                    else {
+                        sum += 10;
+                        skip = 1;
+                    }
+                    break;
+                case 'C':
+                    if (next!=null && next == 'D') {
+                        sum += 400;
+                        skip = 2;
+                    }else if (next!=null && next == 'M'){
+                        sum += 900;
+                        skip = 2;
+                    }
+                    else {
+                        sum += 100;
+                        skip = 1;
+                    }
+                    break;
+                case 'D':
+                    sum += 500;
+                    skip = 1;
+                    break;
+                case 'M':
+                    sum +=1000;
+                    skip = 1;
+                    break;
+                case 'V':
+                    sum+=5;
+                    skip = 1;
+                    break;
+                case 'L':
+                    sum+=50;
+                    skip = 1;
+                    break;
+
+
+            }
+            s = s.substring(skip);
+
+        }
+        return sum;
+    }
+    public static void runTest(){
+        RomanToInteger rti = new RomanToInteger();
+        System.out.println(rti.romanToInt("III"));
+        System.out.println(rti.romanToInt("VIII"));
+        System.out.println(rti.romanToInt("LVIII"));
+        System.out.println(rti.romanToInt("MCMXCIV"));
+    }
+}
