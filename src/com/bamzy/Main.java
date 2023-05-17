a1da2d65d3c5aff8a6a420f872e0c0d56f915788
Bamdad <bamdad@mortgageautomator.com>
2023-05-17 12:40:59 -0400

    not really feeling this solution

--- src/com/bamzy/Main.java
+++ src/com/bamzy/Main.java
@@ -1,23 +1,13 @@
 package com.bamzy;
 
 
-import com.bamzy.amazon.LongestUniqueStringSplit;
-import com.bamzy.amazon.OverlappingRectangles;
-import com.bamzy.amazon.WeightGainDiet;
-import com.bamzy.crack_coding_interview.OneAway;
-import com.bamzy.crack_coding_interview.RotateMatrix;
-import com.bamzy.crack_coding_interview.URLify;
-import com.bamzy.google.*;
-import com.bamzy.netflix.MagicTriplet;
-import java.util.*;
-import java.io.*;
 
 public class Main {
 
 
     public static void main (String[] args) {
 
-        ContainerWithMostWater.runTest();
+        MaximumTastingOfCandyBasket.runTest();
     }
 
 }
